c7c0e6d4930a30e1864f111e4372b93e4058e469
cowgoat88 <kgoetz32@gmail.com>
2016-01-15 22:44:34 -0800

    Update main.py
    CHANGED 
    board randomization
    finish line
    ADDED
    class player
    main loop

--- main.py
+++ main.py
@@ -11,18 +11,24 @@ size = 10
 board = [[0 for x in xrange(size)] for y in xrange(size)]
 
 
+
+'''
+INITIALIZE A PLAYABLE BOARD
+v1.0
+'''
 def init_board(size):
 	
 	for x in xrange(size):
 		for y in xrange(size):
-			board[x][y] = random.choice([0,1])
-	board[0][size] = 2
+			board[x][y] = random.choice([0,0,0,1])	# RANDOM CHOICE FROM 
+	board[size-1][size-1] = 2
 	return board
 
 
 board = init_board(size)
 for x in board:
 	print x
+
 
 def search(x, y):
 	if board[x][y] == 2:
@@ -50,3 +56,64 @@ def search(x, y):
 	
 
 
+'''
+CLASS "PLAYER" PLAYS THROUGH BOARD TO FINISH CONDITION
+v0.0
+'''			
+class player():
+	
+	def __init__(self):
+		self.position = [0,0] # Start position.
+		
+	moves = {
+		'w':[0,1],
+		'a':[-1,0],
+		's':[0,-1],
+		'd':[1,0]
+		}	
+		
+	def player_move(self):
+		
+		moves = {
+			'w':[0,1],
+			'a':[-1,0],
+			's':[0,-1],
+			'd':[1,0]
+			}	
+		
+		move = raw_input('MOVE : ',)
+		try:
+			self.position = [sum(x) for x in zip(self.position,moves[move])]
+			if self.position <= size:
+				pass
+			else:
+				print 'EDGE!'
+		except KeyError:
+			print 'Invalid KEY!'
+			self.player_move()
+			
+	
+	def position(self):
+		return self.position
+		
+		
+		
+
+'''
+INITIALIZE PLAYER
+'''
+wilder = player()
+
+'''
+MAIN LOOP
+prototype v0.0
+'''
+WIN = False
+while WIN == False:
+
+	wilder.player_move()
+	print wilder.position
+	if board[wilder.position[0]][wilder.position[1]]== 2:
+		WIN = True
+		print 'You found the exit! You won a Maze Race!'
+	
