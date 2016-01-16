4d0850379a42c6d101b8161a5685968e756b6a9d
shipskin <shipskin@gmail.com>
2016-01-15 21:38:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -23,5 +23,29 @@ board = init_board(size)
 for x in board:
 	print x
 
-			
+def search(x, y):
+	if board[x][y] == 2:
+		print 'found at %d, %d' % (x, y)
+		return True
+	elif board[x][y] == 1:
+		print 'wall at %d, %d' % (x, y)
+		return False
+	elif board[x][y] == 3:
+		print 'visited %d, %d' % (x, y)
+		return False
+	
+	print 'visiting %d, %d' % (x, y)
+	
+	# mark as visited
+	board [x][y] = 3
+	
+	# explore neighbors clockwise starting on the right
+	if ((x < len(board) -1 and search(x+1, y))
+		or (y > 0 and search(x, y-1))
+		or (x > 0 and search(x-1, y))
+		or (y < len(board)-1 and search(x, y+1))):
+		return True
+	return False
+	
+
 
